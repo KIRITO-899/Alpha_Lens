9eabb80388671cdde0a2858b5f0458b6dc47834b
harthik200620 <harthik200620@gmail.com>
2026-05-12 18:23:32 +0530

    Remove temporary Alpha Lens launcher script

--- .tmp/start_alpha_lens.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "C:\Project rohan\Alpha_Lens"
-"C:\Project rohan\Alpha_Lens\.alpha-venv\Scripts\python.exe" "C:\Project rohan\Alpha_Lens\backend\app.py" > "C:\Project rohan\Alpha_Lens\server.log" 2> "C:\Project rohan\Alpha_Lens\server.err.log"
